055dbfa151a6e67174a97da051914540fe8f040e
John Keyes <jkeyes@John.local>
2016-04-29 23:28:35 +0100

    Making it listen to a specific port just to see.

--- server.js
+++ server.js
@@ -1,2 +1,14 @@
 var QuizBot = require('./index.js');
 var myQuizBot = new QuizBot(process.env.SLACKBOT_TOKEN);
+
+var http = require('http'); 
+var url = require('url'); 
+
+http.createServer(function (req, res) { 
+  console.log("Request: " + req.method + " to " + req.url); 
+  res.writeHead(200, "OK"); 
+  res.write("OK"); 
+  res.end(); 
+}).listen(process.env.PORT || 8000); 
+
+console.log("Ready...");
